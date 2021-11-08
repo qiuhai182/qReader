53efd92ecda1679dff98439fab5546cd553226b1
zqj <1954674324@qq.com>
2021-11-08 09:54:33 +0000

    update library/userdata.hpp.

--- library/userdata.hpp
+++ library/userdata.hpp
@@ -147,7 +147,7 @@ int del_user(const string &user_id)
 			 << " LINE  " << __LINE__ << endl;
 		return -1;
 	}
-	string cond = "where userId = \'" + user_id + "\';
+	string cond = "where userId = \'" + user_id + "\'";
 	auto res = conn->query<UserInfoTable>(cond);
 	if (res.size() == 0)
 		return -3; //账号不存在
