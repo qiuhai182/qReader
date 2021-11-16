8d382ede659ca1282a9eac249d4aab3ab9d247d7
zqj <1954674324@qq.com>
2021-11-16 09:38:25 +0000

    update library/bookdata.hpp.

--- library/bookdata.hpp
+++ library/bookdata.hpp
@@ -209,7 +209,7 @@ int up_book(const string &book_id, const string &book_name,
 	change[5]   = book_intro 	== "" 	? "" : " bookIntro = \'" 	+ book_intro + "\'  ";
     string cond = "update BookInfoTable set "  ;
     for(int i = 0 ;i < 6;i++){
-        cond.append(change[0]);
+        cond.append(change[i]);
         if(change[i] != "" && i != 5)
             cond.append(" ,");
     }
