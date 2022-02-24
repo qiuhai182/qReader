da732b89aca552a2f1549b56156a9220c4e3d548
qiuhai <2810617217@qq.com>
2022-02-24 20:50:38 +0800

    上传最新可执行文件、代码

--- library/public/Times.hpp
+++ library/public/Times.hpp
@@ -137,12 +137,10 @@ namespace Times
         string preHour = hour > 9 ? " " : "0";
         string preMinute = minute > 9 ? " " : "0";
         string preSecond = second > 9 ? " " : "0";
-        // string timeString = to_string(year) + "-" + preMonth + to_string(month) + "-" + preDay + 
-        //                     to_string(day) + " " + preHour + to_string(hour) + "-" + preMinute + 
-        //                     to_string(minute) + "-" + preSecond + to_string(second);
-        return get_timeStamp(to_string(year) + "-" + preMonth + to_string(month) + "-" + preDay + 
+        string timeString = to_string(year) + "-" + preMonth + to_string(month) + "-" + preDay + 
                             to_string(day) + " " + preHour + to_string(hour) + "-" + preMinute + 
-                            to_string(minute) + "-" + preSecond + to_string(second));
+                            to_string(minute) + "-" + preSecond + to_string(second);
+        return get_timeStamp(timeString);
     }
 
 
